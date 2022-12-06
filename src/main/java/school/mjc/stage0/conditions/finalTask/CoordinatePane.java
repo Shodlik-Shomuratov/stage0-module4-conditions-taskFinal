12371354150ead11725341079d5dc276e00a1fe6
Shodlik Shomuratov <shodlikcoder@gmail.com>
2022-12-06 20:31:59 +0500

    All tasks done!

--- src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
@@ -2,5 +2,20 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class CoordinatePane {
     public void printQuadrant(int x, int y) {
+        String coordinate;
+
+        if (x > 0 && y > 0) {
+            coordinate = "first";
+        } else if (x < 0 && y > 0) {
+            coordinate = "second";
+        } else if (x < 0 && y < 0) {
+            coordinate = "third";
+        } else if (x > 0 && y < 0) {
+            coordinate = "fourth";
+        } else {
+            coordinate = "zero";
+        }
+
+        System.out.println(coordinate);
     }
 }
